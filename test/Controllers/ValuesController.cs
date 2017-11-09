f617da9856bf5048f9df74959018d959292a4065
Alessandro Relati <alessandro.relati@gmail.com>
2017-11-09 21:48:54 +0100

    Archiviazione iniziale del modulo testWebApi

---
+++ test/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using test.Model;
+
+namespace test.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly INoteRepository _noteRepository;
+
+        public ValuesController(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var note = new Note() { Body = "pippo", CreatedOn = DateTime.Now };
+            _noteRepository.AddNote(note);
+            var p = _noteRepository.GetAllNotes();
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ test/Model/Settings.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace test.Model
+{
+    public class Settings
+    {
+        public string ConnectionString;
+        public string Database;
+    }
+
+    public class Note
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Body { get; set; } = string.Empty;
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int UserId { get; set; } = 0;
+    }
+
+    public class NoteContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public NoteContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Note> Notes
+        {
+            get
+            {
+                return _database.GetCollection<Note>("Note");
+            }
+        }
+    }
+
+    public interface INoteRepository
+    {
+        Task<IEnumerable<Note>> GetAllNotes();
+        Task<Note> GetNote(string id);
+        Task AddNote(Note item);
+        Task<DeleteResult> RemoveNote(string id);
+        Task<UpdateResult> UpdateNote(string id, string body);
+
+        // demo interface - full document update
+        Task<ReplaceOneResult> UpdateNoteDocument(string id, string body);
+
+        // should be used with high cautious, only in relation with demo setup
+        Task<DeleteResult> RemoveAllNotes();
+    }
+
+    public class NoteRepository : INoteRepository
+    {
+        private readonly NoteContext _context = null;
+
+        public NoteRepository(IOptions<Settings> settings)
+        {
+            _context = new NoteContext(settings);
+        }
+
+        public async Task<IEnumerable<Note>> GetAllNotes()
+        {
+            return await _context.Notes.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<Note> GetNote(string id)
+        {
+            var filter = Builders<Note>.Filter.Eq("Id", id);
+            return await _context.Notes
+                                 .Find(filter)
+                                 .FirstOrDefaultAsync();
+        }
+
+        public async Task AddNote(Note item)
+        {
+            await _context.Notes.InsertOneAsync(item);
+        }
+
+        public async Task<DeleteResult> RemoveNote(string id)
+        {
+            return await _context.Notes.DeleteOneAsync(
+                         Builders<Note>.Filter.Eq("Id", id));
+        }
+
+        public async Task<UpdateResult> UpdateNote(string id, string body)
+        {
+            var filter = Builders<Note>.Filter.Eq(s => s.Id, id);
+            var update = Builders<Note>.Update
+                                .Set(s => s.Body, body)
+                                .CurrentDate(s => s.UpdatedOn);
+            return await _context.Notes.UpdateOneAsync(filter, update);
+        }
+
+        public async Task<ReplaceOneResult> UpdateNote(string id, Note item)
+        {
+            return await _context.Notes
+                                 .ReplaceOneAsync(n => n.Id.Equals(id)
+                                                     , item
+                                                     , new UpdateOptions { IsUpsert = true });
+        }
+
+        public async Task<DeleteResult> RemoveAllNotes()
+        {
+            return await _context.Notes.DeleteManyAsync(new BsonDocument());
+        }
+
+        public Task<ReplaceOneResult> UpdateNoteDocument(string id, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
